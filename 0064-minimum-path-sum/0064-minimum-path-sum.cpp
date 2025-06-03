885852e2772948ccb306849b9098b406f1587489
PON SELVAKUMAR V <167604821+pon-selvakumar@users.noreply.github.com>
2025-06-03 10:10:32 +0530

    Time: 0 ms (100%), Space: 16.3 MB (57.9%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        vector<vector<int>> DP(grid.size(),vector<int>(grid[0].size()));int su=0;
+        vector<vector<int>> DP(grid.size(),vector<int> (grid[0].size()));int su=0;
         DP[0][0]=grid[0][0];
         for(int i=1;i<grid[0].size();i++){
             DP[0][i]=grid[0][i]+DP[0][i-1];
         }
         for(int j=1; j<grid.size();j++){
-            // DP[j][0]+=grid[j][0];
             DP[j][0]=grid[j][0]+DP[j-1][0];
         }
         for(int i=1;i<grid.size();i++){
             for(int j=1;j<grid[0].size();j++){
-                cout<<DP[i][j]<<" ";
                 DP[i][j]=grid[i][j]+ min(DP[i-1][j],DP[i][j-1]);
             }
-            cout<<"\n";
-        }
-         for(int i=1;i<grid.size();i++){
-            for(int j=1;j<grid[0].size();j++){
-                cout<<DP[i][j]<<" ";
-                // DP[i][j]=grid[i][j]+ min(DP[i-1][j],DP[i][j-1]);
-            }
-            cout<<"\n";
         }
         return DP[grid.size()-1][grid[0].size()-1];
 
