51b680621e73af6dce9556a1d87bc9718fc9d052
PON SELVAKUMAR V <167604821+pon-selvakumar@users.noreply.github.com>
2025-06-04 15:19:16 +0530

    Time: 35 ms (55.96%), Space: 22.1 MB (44.29%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,41 +1,45 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        
-        int n=s.size();
-        string st1="",st2="";int mx=0;
-        // while()i
-        int l,r;
-        for(int i=0;i<n;i++){
-            l=r=i;
-            while(l>=0 and r<n and s[l]==s[r]){
-                    l--;r++;
-             
+
+        int n = s.size();
+        string st1 = "", st2 = "";
+        int mx = 0;
+        int l, r;
+        //For ODD;
+        for (int i = 0; i < n; i++) {
+            l = r = i;
+            while (l >= 0 and r < n and s[l] == s[r]) {
+                l--;
+                r++;
             }
-            int len = r-l-1;string z="";
-            if(len > mx){
-                for(int k=l+1 ; k<r;k++) z+=s[k];
-                 st1=z;
-                 mx=len;
-            }
-            
-        }
-        ////
-        mx=0;
-        for(int i=0 ;i<n;i++){
-            l=i;r=i+1;
-            while(l>=0 and r<n and s[l]==s[r]){
-                l--;r++;
-            }
-            int len = r-l-1;
-            if(len > mx){
-               string z="";
-                for(int m=l+1;m<r;m++) z+=s[m];
-                st2=z;
-                mx=len;
+            int len = r - l - 1;
+            string z = "";
+            if (len > mx) {
+                for (int k = l + 1; k < r; k++)
+                    z += s[k];
+                st1 = z;
+                mx = len;
             }
         }
-        string ans=st1.size()>st2.size()?st1:st2;
+        mx = 0;//for EVEN
+        for (int i = 0; i < n; i++) {
+            l = i;
+            r = i + 1;
+            while (l >= 0 and r < n and s[l] == s[r]) {
+                l--;
+                r++;
+            }
+            int len = r - l - 1;
+            if (len > mx) {
+                string z = "";
+                for (int m = l + 1; m < r; m++)
+                    z += s[m];
+                st2 = z;
+                mx = len;
+            }
+        }
+        string ans = st1.size() > st2.size() ? st1 : st2;
         return ans;
     }
 };
